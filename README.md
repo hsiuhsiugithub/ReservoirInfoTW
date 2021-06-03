ad4994537ee8a84a252c796efe201d36631eee63
EustaceCheng <fbi0258zzz@gmail.com>
2021-06-03 14:36:15 +0800

    完成簡易版水庫水位儀表板

--- README.md
+++ README.md
@@ -8,6 +8,6 @@
 2. 視覺化數據(D3.js)。
 
 提示：
-1. 當前抓取方法為axios
+1. 當前抓取方法為axios(資料來源：[taiwanstat](https://www.taiwanstat.com/waters/latest))
 2. 可依照不同水量做顏色區別
 3. 利用D3.js製作注水動畫([liquidFillGauge](http://bl.ocks.org/brattonc/5e5ce9beee483220e2f6))
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function NewValue() {
     if (Math.random() > .5) {
         return Math.round(Math.random() * 100);
@@ -8,6 +6,21 @@ function NewValue() {
     }
 }
 
+function setColor(config, percentage) {
+    if (percentage < 25) {
+      config.circleColor = "#FF7777";
+      config.textColor = "#FF4444";
+      config.waveTextColor = "#FFAAAA";
+      config.waveColor = "#FFDDDD";
+    }
+    else if (percentage < 50) {
+      config.circleColor = "rgb(255, 160, 119)";
+      config.textColor = "rgb(255, 160, 119)";
+      config.waveTextColor = "rgb(255, 160, 119)";
+      config.waveColor = "rgba(245, 151, 111, 0.48)";
+    }
+  }
+
 axios.get(`https://www.taiwanstat.com/waters/latest`)
     .then((response) => {
         var dataObject = response.data;
@@ -15,33 +28,47 @@ axios.get(`https://www.taiwanstat.com/waters/latest`)
             , '德基水庫', '石岡壩', '日月潭水庫', '霧社水庫', '湖山水庫', '仁義潭水庫', '蘭潭水庫'
             , '白河水庫', '曾文水庫', '烏山頭水庫', '南化水庫', '阿公店水庫', '牡丹水庫'] //21個水庫
 
+
+
+        //初始化圖表
         for (let i = 0; i < Object.keys(dataObject[0]).length; i++) {
+
+            var e = document.createElement('div');
+            e.innerHTML = `
+            <h3 style="text-align: center;">${reservoirList[i]}</h3>
+            <svg id="fillgauge${i}" width="100%" height="200px"></svg>
+            <div>有效蓄水量：${dataObject[0][reservoirList[i]]['volumn']}萬立方公尺</div>
+            <div>更新時間：${dataObject[0][reservoirList[i]]['updateAt']}</div>
+
+            `;
+            e.className = "reservoir";
+            document.getElementById('content').append(e);
+
+        }
+
+        for (let i = 0; i < Object.keys(dataObject[0]).length; i++) {
+
+            //圖像化數據
             console.log(reservoirList[i])
             console.log(dataObject[0][reservoirList[i]])
+
+            var config = liquidFillGaugeDefaultSettings();
+            config.circleThickness = 0.15;
+            setColor(config, dataObject[0][reservoirList[i]]['percentage'])
+          
+            config.textVertPosition = 0.8;
+            config.waveAnimateTime = 1000;
+            config.waveHeight = 0.05;
+            config.waveAnimate = true;
+            config.waveRise = false;
+            config.waveHeightScaling = false;
+            config.waveOffset = 0.25;
+            config.textSize = 0.75;
+            config.waveCount = 3;
+            var gauge = loadLiquidFillGauge("fillgauge"+i, dataObject[0][reservoirList[i]]['percentage'], config);
         }
     },
         (error) => {
             var message = error.response.data.message;
         }
     );
-
-window.onload = function () {
-
-    
-    var config = liquidFillGaugeDefaultSettings();
-    config.circleThickness = 0.15;
-    config.circleColor = "#808015";
-    config.textColor = "#555500";
-    config.waveTextColor = "#FFFFAA";
-    config.waveColor = "#AAAA39";
-    config.textVertPosition = 0.8;
-    config.waveAnimateTime = 1000;
-    config.waveHeight = 0.05;
-    config.waveAnimate = true;
-    config.waveRise = false;
-    config.waveHeightScaling = false;
-    config.waveOffset = 0.25;
-    config.textSize = 0.75;
-    config.waveCount = 3;
-    var gauge = loadLiquidFillGauge("fillgauge", 60.44, config);
-}
